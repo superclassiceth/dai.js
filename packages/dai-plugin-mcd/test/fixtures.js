484b773a48a3fe4b18cb137f57e2f095562f483c
Tyler Sorensen <tyler@makerdao.com>
2019-10-28 14:33:07 -1000

    add bite event to fixtures

--- packages/dai-plugin-mcd/test/fixtures.js
+++ packages/dai-plugin-mcd/test/fixtures.js
@@ -2,7 +2,8 @@ import { MDAI } from '../src';
 
 export function dummyEventData(ilkIdentifier) {
   return [
-    {
+    { 
+      eventType: 'frob',
       dart: '-6000000000000000000',
       dink: '0',
       ilkRate: '2000000000000000000000000000',
@@ -16,6 +17,18 @@ export function dummyEventData(ilkIdentifier) {
         nodes: [{ art: '6849191748702380000', ink: '5336190215977040000' }]
       },
       ilkIdentifier
+    },
+    {
+      eventType: 'bite',
+      ink: '2000000000000000000',
+      art: '9000000000000000000',
+      tab: '9000000000000000000',
+      tx: {
+        transactionHash:
+          '0xbe023a205453b833e65bf29063de8b8b3bd44d2e68c9c079f681ec46a765a63f',
+        txFrom: '0x1ad35418e7b7c5746ea42295a1100480a810256a',
+        era: { iso: '1970-01-01T00:01:20' }
+      }
     }
   ];
 }
@@ -24,13 +37,21 @@ export function formattedDummyEventData(GEM, ilk) {
   return [
     {
       transactionHash:
-        '0xbe023a205453b833e65bf29063de8b8b3bd44d2e68c9c079f681ec46a765a63f',
+        '0x37d1c253f79784313045d3922928f646020d545f17fc3d74a0d68e665db0b394',
       changeInCollateral: GEM(0),
       changeInDai: MDAI(12),
       daiAction: 'wipe',
       time: new Date('1970-01-01T00:01:20'),
       senderAddress: '0x1ad35418e7b7c5746ea42295a1100480a810256a',
       ilk
+    },
+    {
+      transactionHash:
+        '0xbe023a205453b833e65bf29063de8b8b3bd44d2e68c9c079f681ec46a765a63f',
+      changeInCollateral: ETH(0),
+      liquidated: true,
+      time: new Date(Date.now()),
+      senderAddress: '0x1ad35418e7b7c5746ea42295a1100480a810256a'
     }
   ];
 }
